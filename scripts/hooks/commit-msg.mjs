34eee2a1aacf5eb34428ba195aee2858efaadf27
Armin Kunkel <armin.kunkel92@gmail.com>
2025-11-12 22:03:57 +0100

    feat: git hook installs in MonoRepo and cloned fork successfully while installing

--- scripts/hooks/commit-msg.mjs
+++ scripts/hooks/commit-msg.mjs
@@ -2,9 +2,27 @@ import fs from 'node:fs'
 import path from 'node:path'
 
 const hookSource = path.resolve('scripts', 'hooks', 'commit-msg')
-const hookTarget = path.resolve('.git', 'hooks', 'commit-msg')
+const hookTarget = path.resolve('.git')
+const hookTarget2 = path.resolve('.git', 'hooks', 'commit-msg')
 
-fs.copyFileSync(hookSource, hookTarget)
-fs.chmodSync(hookTarget, 0o755)
+const stats = fs.statSync(hookTarget)
+let target = ''
 
-console.log('✅ Git hook installed:', hookTarget)
+if (stats.isFile()) {
+  // MonoRepo was cloned, so read the actual git dir from .git and write to this dir instead
+  const content = fs.readFileSync(hookTarget).toString('utf-8')
+  const relativePath = content.replace('gitdir: ', '')
+  const resolvedPath = path.resolve(relativePath)
+  const absolutePath = path.join(resolvedPath, 'hooks', 'commit-msg').replace('\n', '')
+
+  target = absolutePath
+  fs.copyFileSync(hookSource, absolutePath)
+  fs.chmodSync(absolutePath, 0o755)
+} else {
+  // Submodule was cloned, so write directly to the hooks directory of this project
+  target = hookTarget2
+  fs.copyFileSync(hookSource, hookTarget2)
+  fs.chmodSync(hookTarget, 0o755)
+}
+
+console.log('✅ Git hook installed:', target)
